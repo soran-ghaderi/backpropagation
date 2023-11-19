e7cfb4d36e73aa462372252438dea73d3b6b938b
Soran Ghaderi <sg23454@essex.ac.uk>
2023-11-19 14:12:59 +0000

    feat: Neuron implicitly infers the size of input

--- dev_mlp.py
+++ dev_mlp.py
@@ -4,17 +4,17 @@ from autodiff import Value
 class Neuron:
 
     def __init__(self, inp_no):
-        self.w = [Value(random.uniform(-1, 1)) for _ in range(inp_no)]
+        self.w = None
         self.b = Value(random.uniform(-1, 1))
 
     def __call__(self, x, *args, **kwargs):
-        act = sum(wi*xi for wi, xi in zip(self.w, x)) + self.b
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(len(x))]
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), start=self.b)
         out = act.sigmoid()
-        print(act, out)
         return out
 
 
-x = [2.0, 3.0]
+x = [2.0, 3.0, 4.0]
 n = Neuron(2)
 print("here:", n(x))
 
